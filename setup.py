733405360de3e575cd74bc7f754d35da106aa7d9
cherseroff <proffitm1nd@gmail.com>
2024-11-23 06:09:56 +0300

    renamed

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='module-telegram_notificator',
+    name='telegram_notificator',
     version='1.0',
     packages=find_packages(),
     install_requires=[
